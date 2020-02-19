df498e8b10b34daa040a52d62d465500e463b5de
Yaroslav Ilin <neznayu.ucoz.org@gmail.com>
2020-02-19 20:19:50 +0300

    Update BinarySearch.java
    write first Version Pre and Pos
    
    Co-Authored-By: fmyb <fmyb@users.noreply.github.com>

--- Binary-Search/src/search/BinarySearch.java
+++ Binary-Search/src/search/BinarySearch.java
@@ -11,19 +11,35 @@ public class BinarySearch {
         for (int i = 1; i < args.length; i++) {
             searchArray[i - 1] = Integer.parseInt(args[i]);
         }
+//        Pre: left = -1, right = length
+//        Inv: left < index of findVariable <= right && Arr[left] > findVariable >= Arr[right]
         int ans = searchRecursion(searchArray, findVariable, -1, searchArray.length);
         System.out.println(ans == -1 ? searchArray.length : ans);
     }
 
     public static int searchCycle(int[] searchArray, int findVariable, int left, int right) {
+//        Pre: left < index of find Variable <= right && Arr[left] > find Variable >= Arr[right]
         while (left + 1 < right) {
+//            Post: left < index of find Variable <= right && left + 1 < right && Arr[left] > find Variable >= Arr[right]
+//            Pre: left < index of find Variable <= right && left + 1 < right && Arr[left] > find Variable >= Arr[right]
             int middle = (left + right) / 2;
+//            Post: left < index of find Variable <= right && left + 1 < right && left < middle < right
+//              && Arr[left] > find Variable >= Arr[right]
+//            Pre: left < index of find Variable <= right && left + 1 < right && left < middle < right
+//              && Arr[left] > find Variable >= Arr[right]
             if (searchArray[middle] <= findVariable) {
+//                Pre: left < index of find Variable <= right && left + 1 < right && left < middle < right
+//                  && Arr[left] > find Variable >= Arr[right] && Arr[middle] <= find Variable
                 right = middle;
+//                Post: Arr[left] > find Variable >= Arr[right] && left < find Variable <= right = middle
             } else {
+//                Pre: left < index of find Variable <= right && left + 1 < right && left < middle < right
+//                  && Arr[left] > find Variable >= Arr[right] && Arr[middle] > find Variable
                 left = middle;
+//                Post: Arr[left] > find Variable >= Arr[right] && left = middle < find Variable <= right
             }
         }
+        // Post: left + 1 = find Variable = right &&  Arr[left] > find Variable >= Arr[right]
         return right;
     }
 
